693178ee7fea65b7b0028ce527a82116624662a1
Kriss Strikis <kstrikis@gmail.com>
2025-01-25 14:42:41 -0500

    fix: add return type to AdminDashboard useEffect

--- src/components/dashboard/AdminDashboard.tsx
+++ src/components/dashboard/AdminDashboard.tsx
@@ -10,7 +10,7 @@ interface Props {
 export function AdminDashboard({ className }: Props): React.ReactElement {
   logger.methodEntry("AdminDashboard");
 
-  useEffect(() => {
+  useEffect((): (() => void) => {
     logger.info("AdminDashboard mounted");
     return () => {
       logger.info("AdminDashboard unmounted");
